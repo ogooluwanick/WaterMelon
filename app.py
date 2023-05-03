154df3313ffd61312b302f32baf4c8b1430679e2
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2023-05-04 00:14:29 +0100

    thanks page

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from flask import Flask,request, render_template
 
 app = Flask(__name__)
 
+app.debug = True
+
+
 @app.route('/')
 def index():
-    return render_template('form.html')
+        return render_template('form.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+        name = request.form['name']
+        bal = request.form['bal']
+        return render_template('thanks.html', name=name, bal=bal)
