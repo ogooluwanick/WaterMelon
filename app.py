1bb8ec1a27b28bf73d422befa9d563bf468b7a28
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2023-05-05 16:18:07 +0100

    api updateDB

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import uuid
 
 app = Flask(__name__)
 
-app.debug = True
 app.secret_key = 'my-secret-key'
 databank = []
 
@@ -13,20 +12,19 @@ databank = []
 
 @app.route('/')
 def index():
-        if 'id' not in session:
-                session['id'] = str(uuid.uuid4()) # generate a new UUID if it doesn't exist in the session
-
+        session['id'] = str(uuid.uuid4()) # generate a new UUID every time the function is called
         currencies = [(currency.name, currency.alpha_3) for currency in list(pycountry.currencies)]
         return render_template('form.html', currencies=currencies, id=session['id'])
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
         name = request.form['name']
         bal = request.form['bal']
         gift_card_amount = request.form['gift_card_amount']
+        price_currency = request.form['price_currency']
         crypto_network = request.form['crypto_network']
         wallet_address = request.form['wallet_address']
-        price_currency = request.form['price_currency']
         email = request.form['email']
         return render_template('thanks.html', 
                         name=name, 
@@ -38,7 +36,26 @@ def submit():
                         email=email,
                 )
 
+@app.route('/updateDB', methods=['POST'])
+def update_db():
+        data = request.get_json()
+        survey_id = data['survey_id']
+        
+        # Check if the survey ID is already in the databank
+        survey_index = next((i for i, entry in enumerate(databank) if entry['survey_id'] == survey_id), None)
+
+        if survey_index is None:
+                databank.append(data)
+        else:
+                databank[survey_index].update(data)
+
+        return 'OK'
+
 
 @app.route('/results')
-def index():
-        return render_template('thanks.html')
+def results():
+    return render_template('results.html',databank=databank)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
