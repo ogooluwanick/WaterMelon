5cda5a083e76b52e3481607cf2b9c8183eb0afde
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2023-05-05 03:02:10 +0100

    api update

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
-from flask import Flask,request, render_template
+from flask import Flask,request, render_template, session
+import pycountry
+import uuid
 
 
 app = Flask(__name__)
 
 app.debug = True
+app.secret_key = 'my-secret-key'
+databank = []
+
 
 
 @app.route('/')
 def index():
-        return render_template('form.html')
+        if 'id' not in session:
+                session['id'] = str(uuid.uuid4()) # generate a new UUID if it doesn't exist in the session
+
+        currencies = [(currency.name, currency.alpha_3) for currency in list(pycountry.currencies)]
+        return render_template('form.html', currencies=currencies, id=session['id'])
 
 @app.route('/submit', methods=['POST'])
 def submit():
         name = request.form['name']
         bal = request.form['bal']
-        return render_template('thanks.html', name=name, bal=bal)
+        gift_card_amount = request.form['gift_card_amount']
+        crypto_network = request.form['crypto_network']
+        wallet_address = request.form['wallet_address']
+        price_currency = request.form['price_currency']
+        email = request.form['email']
+        return render_template('thanks.html', 
+                        name=name, 
+                        bal=bal, 
+                        gift_card_amount=gift_card_amount,
+                        crypto_network=crypto_network,
+                        wallet_address=wallet_address,
+                        price_currency=price_currency,
+                        email=email,
+                )
+
+
+@app.route('/results')
+def index():
+        return render_template('thanks.html')
